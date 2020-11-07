3113ffdb0b1a30a5b1dd3a44eff665bde256df21
Ignacio Barrera <ignaciobarreragarcia@gmail.com>
2020-11-07 18:06:21 -0300

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand">V8</a>
+        <button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <!-- MENU DE NAVEGACION -->
+          <ul class="nav navbar-nav ml-auto" id="menu-v8">
+            <li class="nav-item" role="presentation">
+              <!-- REEMPLAZAR CON ELEMENTO ROUTER LINK A CON RUTA NOMBRADA -->
+              <router-link :to="{name: 'Portada'}">Portada</router-link>|
+            </li>
+            <li class="nav-item" role="presentation">
+              <!-- REEMPLAZAR CON ELEMENTO ROUTER LINK A CON RUTA NOMBRADA -->
+              <router-link :to="{name: 'SobreMi'}">Sobre Mí</router-link>|
+            </li>
+            <li class="nav-item" role="presentation">
+              <!-- REEMPLAZAR CON ELEMENTO ROUTER LINK A CON RUTA ESTÁTICA -->
+              <router-link to="/Contacto">Contacto</router-link>|
+            </li>
+            <li class="nav-item" role="presentation">
+              <!-- REEMPLAZAR CON ELEMENTO ROUTER LINK A RUTA ESTÁTICA -->
+              <router-link to="/post/1">Último post</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
